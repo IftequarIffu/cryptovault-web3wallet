f73a6958da50e067173890d1ab290344a452ad6b
IftequarIffu <iftequar.154@gmail.com>
2024-08-01 09:01:53 +0530

    Added constants

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+// export const RPC_URLS = {
+//     "Ethereum": "http://abcd.com",
+//     "Solana": "http://wxyz.com"
+// }
+
+export const supportedNetworks = {
+  Ethereum: { name: "Ethereum", symbol: "ETH", color: "bg-blue-500" },
+  Solana: { name: "Solana", symbol: "SOL", color: "bg-purple-500" },
+};
