7248ee75ccd9fd5bb796ec0fc0a3c32249861b81
IftequarIffu <iftequar.154@gmail.com>
2024-08-10 09:47:53 +0530

    Modified: ethUtils.ts

---
+++ lib/ethUtils.ts
@@ -0,0 +1,60 @@
+import { ETH_DECIMAL_PLACES, ETHERSCAN_API_KEY, SEPOLIA_ETHERSCAN_URL } from "./constants";
+
+export const convertWeiToEth = (wei: number) => {
+    return wei/(10**ETH_DECIMAL_PLACES);
+}
+
+
+export const getTopThreeTxsOfAnEthAddress = async(address: string, etherscanUrl: string, apiKey: string = ETHERSCAN_API_KEY) => {
+    
+    console.log(`API Key: ${apiKey}`)
+    let txsList: any[] = []
+    try {
+        const axios = require('axios');
+
+    let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `${etherscanUrl}/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${apiKey}`,
+    headers: { }
+    };
+
+    const response = await axios.request(config)
+    console.log("response: ", response)
+    const result = response.result
+    console.log("result: ", result)
+
+
+    for(let item of result) {
+        if(txsList.length == 3){
+            break;
+        }
+        txsList.push({
+            sender: item.from,
+            receiver: item.to,
+            amount: convertWeiToEth(parseInt(item.value)),
+            txTimestamp: parseInt(item.timeStamp)
+        })
+    }
+
+    // result.forEach((item: any) => {
+    //     if(txsList.length === 3){
+    //         break;
+    //     }
+    //     txsList.push({
+    //         sender: item.from,
+    //         receiver: item.to,
+    //         amount: convertWeiToEth(parseInt(item.value)),
+    //         txTimestamp: parseInt(item.timeStamp)
+    //     })
+    // })
+
+    } catch (error: any) {
+        console.error(`Error occured while getting Txs for address: ${address}: `,  error)
+        throw new Error(error.message)
+    }
+
+    finally{
+        return txsList
+    }
+}
