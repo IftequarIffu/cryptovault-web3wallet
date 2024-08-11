5ed321fec0077f63b2c47d8714948f5a64f06313
IftequarIffu <iftequar.154@gmail.com>
2024-08-11 09:37:57 +0530

    Modified: types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,26 @@
-export type NetworkType = "Ethereum" | "Solana";
+export type NetworkType = "Ethereum" | "Solana" | "ETH Sepolia" | "SOL Devnet";
 import { Account as AccountType } from "@prisma/client";
 
 export type ClientAccountType = AccountType & { balance: number };
 
+export type TransactionType = {
+  type: "send" | "receive" | "swap";
+  amount: string;
+  to?: string;
+  from?: string;
+  date: string;
+};
+
+export type TokenType = {
+  name: string;
+  symbol: string;
+  balance: number;
+  value: number;
+};
+
 export type WalletContextType = {
   selectedNetwork: NetworkType;
-  accounts: ClientAccountType[];
+  accounts: any[];
   selectedAccount: string;
   changeSelectedAccount: (accountId: string) => void;
   activeTab: "tokens" | "nfts";
@@ -13,14 +28,22 @@ export type WalletContextType = {
   transferFromAccountId: string | null;
   isAddAccountModalOpen: boolean;
   mnemonic: string;
+  transactions: TransactionType[];
+  tokens: TokenType[];
+  areValuesDecrypted: boolean;
+  password: string | null;
+  setWalletPassword: (password: string) => void;
   changeNetwork: (network: NetworkType) => void;
   setActiveTab: (tab: "tokens" | "nfts") => void;
   toggleGenericTransferModal: () => void;
   setTransferFromAccountModal: (accountId: string | null) => void;
   toggleAddAccountModal: () => void;
-  setMnemonic: (mnemonic: string) => void;
+  setMnemonicString: (mnemonic: string) => void;
   clearMnemonic: () => void;
-  addAccount: (account: ClientAccountType) => void;
-  setAllAccounts: (accounts: ClientAccountType[]) => void;
+  addAccount: (account: any) => void;
+  setAllAccounts: (accounts: any[]) => void;
   updateAccountBalance: (id: string, balance: number) => void;
+  setAllTokens: (newTokens: TokenType[]) => void;
+  setValuesToDecryptedState: () => void;
+  setValuesToEncryptedState: () => void;
 };
