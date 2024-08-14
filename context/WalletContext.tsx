0ec47010a394408c0d482502fedb02a26bbf66fd
IftequarIffu <iftequar.154@gmail.com>
2024-08-14 09:41:47 +0530

    Modified WalletContext

--- context/WalletContext.tsx
+++ context/WalletContext.tsx
@@ -8,9 +8,30 @@ import React, {
 } from "react";
 // import { useUser } from "@clerk/nextjs";
 // import { Account } from "@prisma/client";
-import { WalletContextType, NetworkType } from "@/lib/types";
+import { WalletContextType, NetworkType, TokenType } from "@/lib/types";
 import { ClientAccountType } from "@/lib/types";
-import { getAccountsOfAUserWallet } from "@/actions/walletActions";
+import {
+  get3RecentTransactionsOfUserWallet,
+  getAccountsOfAUserWallet,
+  getTokens,
+} from "@/actions/walletActions";
+import { generateMnemonic } from "bip39";
+import { useUser } from "@clerk/nextjs";
+import { HDNodeWallet, Mnemonic } from "ethers";
+import { Keypair } from '@solana/web3.js';
+import * as bip39 from 'bip39';
+import { derivePath } from 'ed25519-hd-key';
+import nacl from 'tweetnacl';
+import { getBalanceOfAnAddress } from "@/actions/balance";
+import { getMnemonicAfterGeneratingIt,
+   getMnemonicFromLocalStorage, 
+   getEthAndSolAccountsAfterGenerating,
+   updateUnsafeMetaDataOfUserForEthAndSol,
+   getAccountsFromLocalStorage,
+   encrypt
+   } from "@/lib/utils"; 
+
+
 // import { Account } from "@prisma/client";
 
 const WalletContext = createContext<WalletContextType | undefined>(undefined);
@@ -20,11 +41,13 @@ export function WalletContextProvider({
 }: {
   children: React.ReactNode;
 }) {
+
+  const {user, isLoaded} = useUser()
+
   const [selectedNetwork, setSelectedNetwork] =
     useState<NetworkType>("Ethereum");
-  const [accounts, setAccounts] = useState<ClientAccountType[]>(
-    getAccountsOfAUserWallet("userId")
-  );
+  const [accounts, setAccounts] = useState<any[]>([]);
+  // const [accounts, setAccounts] = useState()
   const [activeTab, setActiveTab] = useState<"tokens" | "nfts">("tokens");
   const [isGenericTransferModalOpen, setIsGenericTransferModalOpen] =
     useState(false);
@@ -33,19 +56,18 @@ export function WalletContextProvider({
   >(null);
   const [isAddAccountModalOpen, setIsAddAccountModalOpen] = useState(false);
   const [mnemonic, setMnemonic] = useState("");
-  const [selectedAccount, setSelectedAccount] = useState("account-0");
 
-  // const { user } = useUser();
+  const [selectedAccount, setSelectedAccount] = useState("Eth Account");
+  const [tokens, setTokens] = useState(getTokens());
 
-  // useEffect(() => {
-  //   if (user) {
-  //     // In a real application, you would fetch the user's accounts from your backend here
-  //     setAccounts([
-  //       { id: "1", name: "Main Account", balance: 0 },
-  //       { id: "2", name: "Savings", balance: 0 },
-  //     ]);
-  //   }
-  // }, [user]);
+  const [areValuesDecrypted, setAreValuesDecrypted] = useState(false)
+  
+  const [password, setPassword] = useState<string|null>(null)
+
+
+  const [transactions, setTransactions] = useState(
+    get3RecentTransactionsOfUserWallet("abcd")
+  );
 
   const changeNetwork = useCallback((network: NetworkType) => {
     setSelectedNetwork(network);
@@ -70,16 +92,21 @@ export function WalletContextProvider({
     setMnemonic("");
   }, []);
 
-  const addAccount = useCallback((account: ClientAccountType) => {
-    setAccounts((prev) => [...prev, account]);
+  const setMnemonicString = useCallback((newMnemonic: string) => {
+    console.log("New Mnemonic in setMnemoniStaring: ", newMnemonic)
+    setMnemonic(newMnemonic);
   }, []);
 
-  const setAllAccounts = useCallback((accounts: ClientAccountType[]) => {
+  const addAccount = useCallback((account: any) => {
+    setAccounts((prev: any) => [...prev, account]);
+  }, []);
+
+  const setAllAccounts = useCallback((accounts: any[]) => {
     setAccounts(accounts);
   }, []);
 
   const updateAccountBalance = useCallback((id: string, balance: number) => {
-    setAccounts((prev) =>
+    setAccounts((prev: any[]) =>
       prev.map((account) =>
         account.id === id ? { ...account, balance } : account
       )
@@ -90,6 +117,287 @@ export function WalletContextProvider({
     setSelectedAccount(accountId);
   }, []);
 
+  const setAllTokens = useCallback((newTokens: TokenType[]) => {
+    setTokens(newTokens);
+  }, []);
+
+  const setValuesToDecryptedState = useCallback(() => {
+    setAreValuesDecrypted(true);
+  }, []);
+
+  const setValuesToEncryptedState = useCallback(() => {
+    setAreValuesDecrypted(false);
+  }, []);
+
+  const setWalletPassword = useCallback((password: string) => {
+    setPassword(password);
+  }, []);
+
+  // useEffect(() => {
+  //   if (!mnemonic) {
+  //     const generatedMnemonic = generateMnemonic();
+  //     console.log("Generating mnemonic in context:", generatedMnemonic);
+  //     setMnemonic(generatedMnemonic);
+  //   }
+  // }, []);
+
+  const initializeLocalStorageWithMnemonicAndEthAccountAndSolAccount = (newMnemonic: string, accountsArr: any) => {
+    localStorage.setItem(`user_details_${user?.id}`, JSON.stringify({
+      mnemonic: newMnemonic,
+      accounts: accountsArr
+    }))
+  }
+
+  const getEthDerivationPath = (totalEthAccounts: number) => {
+    return `m/44'/60'/0'/0/${totalEthAccounts}`;
+  };
+
+  const getSolanaDerivationPath = (totalSolanaAccounts: number) => {
+    return `m/44'/501'/${totalSolanaAccounts}'/0'`;
+  };
+
+  const createEthAccount = (totalEthereumAccountsCreatedByUserSoFar: number, newMnemonic: string, name:string = "Eth Account 1",) => {
+    const mnemonicObject = Mnemonic.fromPhrase(newMnemonic);
+    const path = getEthDerivationPath(totalEthereumAccountsCreatedByUserSoFar)
+    console.log(path)
+    const hdNodeWallet = HDNodeWallet.fromMnemonic(mnemonicObject, path);
+
+    const accountPayload = {
+      id: name,
+      name: name,
+      balance: 0,
+      address: hdNodeWallet.address,
+      publicKey: hdNodeWallet.publicKey,
+      privateKey: hdNodeWallet.privateKey,
+      network: 'Ethereum',
+      derivationPath: path,
+    }
+    
+    return accountPayload;
+  }
+
+  const createSolAccount = async(totalSolanaAccountsCreatedByUserSoFar: number, newMnemonic: string, name:string = "Sol Account 1",) => {
+    
+    console.log("Mnemonic in createSolAccount: ", newMnemonic)
+    const seed = await bip39.mnemonicToSeed(newMnemonic);
+    const derivationPath = getSolanaDerivationPath(0)
+    const derivedSeed = derivePath(derivationPath, seed.toString('hex')).key;
+
+    const keyPair = nacl.sign.keyPair.fromSeed(derivedSeed);
+    const solanaKeypair = Keypair.fromSecretKey(Uint8Array.from(keyPair.secretKey));
+    const address = solanaKeypair.publicKey.toBase58()
+    
+
+    const accountPayload = {
+      id: name,
+      name: name,
+      balance: 0,
+      address: address,
+      publicKey: address,
+      privateKey: solanaKeypair.secretKey,
+      network: 'Solana',
+      derivationPath: derivationPath,
+    }
+    
+    return accountPayload;
+  }
+
+  const updateTotalEthAccountsCreated = async(user: any, totalEthAccounts:number) => {
+    await user.update({
+      unsafeMetadata: {
+        ...user.unsafeMetadata,
+        totalEthereumAccountsCreated: totalEthAccounts+1,
+      },
+    });
+  }
+
+  const updateTotalSolAccountsCreated = async(user: any, totalSolAccounts:number) => {
+    await user.update({
+      unsafeMetadata: {
+        ...user.unsafeMetadata,
+        totalSolanaAccountsCreated: totalSolAccounts+1,
+      },
+    });
+  }
+
+  const updateAccountMetaData = async(user: any) => {
+    await user.update({
+      unsafeMetadata: {
+        ...user.unsafeMetadata,
+        totalSolanaAccountsCreated: 1,
+        totalEthereumAccountsCreated: 1
+      },
+    });
+  }
+
+  // useEffect(() => {
+
+  //   const UseEffectFunction = async() => {
+  //   // if (user?.id) {
+  //   //   setSelectedAccount("Eth Account 1")
+  //   //   // setSelectedAccount("account-0")
+  //   //   let storedData = JSON.parse(localStorage.getItem(`user_details_${user?.id}`) || "{}");
+  //   //   if (storedData.mnemonic) {
+  //   //     console.log("Retrieved stored mnemonic for user:", user.id);
+  //   //     setMnemonic(storedData.mnemonic);
+  //   //     if(storedData.accounts.length != 0){
+
+  //   //       let accountsWithUpdatedBalances:any = []
+  //   //       await storedData.accounts.map(async(item: any) => {
+  //   //       const balance = await getBalanceOfAnAddress(item.address, item.network)
+  //   //       console.log("balance: ", balance)
+  //   //       accountsWithUpdatedBalances.push({...item, balance: balance})
+  //   //     })
+
+        
+  //   //     console.log("Accounts with updated balances", accountsWithUpdatedBalances)
+  //   //     localStorage.setItem(`user_details_${user?.id}`, JSON.stringify({mnemonic: storedData.mnemonic, accounts: accountsWithUpdatedBalances}))
+  //   //     setAccounts(accountsWithUpdatedBalances);
+  //   //     }
+  //   //   } else {
+  //   //     const newMnemonic = generateMnemonic();
+  //   //     console.log("Generating new mnemonic for user:", user.id);
+  //   //     setMnemonic(newMnemonic);
+  //   //     const ethAccountPayload = createEthAccount(0, newMnemonic)
+  //   //     const solAccountPayload = await createSolAccount(0, newMnemonic)
+  //   //     const accountsArr = [ethAccountPayload, solAccountPayload]
+  //   //     // setAccounts(getAccountsOfAUserWallet("abcd"))
+  //   //     try {
+  //   //       await initializeLocalStorageWithMnemonicAndEthAccountAndSolAccount(newMnemonic, accountsArr)
+  //   //       await updateAccountMetaData(user)
+  //   //     } catch (error) {
+  //   //       console.log("Error in Wallet Context UseEffect: ", error)
+          
+  //   //     }
+  //   //   }
+  //   // }
+
+  //   if(user?.id) {
+
+  //     // Mnemonic Functions
+  //     const storedData = JSON.parse(localStorage.getItem(`user_details_${user.id}`) || "{}")
+  //     let generatedMnemonic = '';
+
+  //     if(!storedData.mnemonic){
+  //       generatedMnemonic = getMnemonicAfterGeneratingIt(user)
+  //       console.log("Mnemonic after generating: ", generatedMnemonic)
+
+  //       // encryptStringWithPassword(generatedMnemonic, pass)
+  //       setMnemonicString(generatedMnemonic)
+  //     }
+  //     else{
+  //       const mnemonicFromLocalStorage = getMnemonicFromLocalStorage(user)
+  //       setMnemonicString(mnemonicFromLocalStorage)
+  //     }
+
+
+  //     // Account Functions
+  //     let generatedAccounts = []
+  //     if(!storedData.accounts || storedData.accounts.length == 0){
+  //       generatedAccounts =  await getEthAndSolAccountsAfterGenerating(user, generatedMnemonic)
+  //       setAccounts(generatedAccounts)
+  //       updateUnsafeMetaDataOfUserForEthAndSol(user, 2, 2)
+  //     }
+  //     else{
+  //       const accountsFromLocalStorage = getAccountsFromLocalStorage(user)
+  //       setAccounts(accountsFromLocalStorage)
+  //     }
+
+  //     // Write similar logic for Tokens, NFTs and Recent Tansactions
+      
+  //   }
+
+  // }
+
+  //   UseEffectFunction()
+  // }, [user]);
+
+  useEffect(() => {
+
+    const SetMnemonicUseEffectFunction = async() => {
+  
+      if(user?.id) {
+  
+        // Mnemonic Functions
+        const storedData = JSON.parse(localStorage.getItem(`user_details_${user.id}`) || "{}")
+        let generatedMnemonic = '';
+  
+        if(!storedData.mnemonic){
+          generatedMnemonic = getMnemonicAfterGeneratingIt(user)
+          console.log("Mnemonic after generating in Wallet Context: ", generatedMnemonic)
+  
+          // encryptStringWithPassword(generatedMnemonic, pass)
+          setMnemonicString(generatedMnemonic)
+          console.log("Mnemonic string set 1: ", generatedMnemonic)
+
+        }
+        else{
+          if(areValuesDecrypted == false) {
+            const mnemonicFromLocalStorage = getMnemonicFromLocalStorage(user)
+            setMnemonicString(mnemonicFromLocalStorage)
+            console.log("Mnemonic string set 2: ", mnemonicFromLocalStorage)
+          }
+          
+
+        }
+        
+      }
+  
+    }
+
+    SetMnemonicUseEffectFunction()
+
+    // if (isLoaded && mnemonic) {
+    //   console.log("Mnemonic in MnemonicDisplay:", mnemonic);
+    //   // setIsLoading(false);
+    // }
+  }, [isLoaded]);
+
+
+
+  useEffect(() => {
+
+    const SetAccountsUseEffectFunction = async() => {
+  
+      if(user?.id && mnemonic) {
+  
+        console.log("Yoo! mnemonic: ", mnemonic)
+
+        const storedData = JSON.parse(localStorage.getItem(`user_details_${user.id}`) || "{}")
+  
+        // Account Functions
+        let generatedAccounts = []
+        if(!storedData.accounts || storedData.accounts.length == 0){
+          generatedAccounts =  await getEthAndSolAccountsAfterGenerating(user, mnemonic)
+          setAllAccounts(generatedAccounts)
+          updateUnsafeMetaDataOfUserForEthAndSol(user, 2, 2)
+        }
+        else{
+          if(areValuesDecrypted == false) {
+            const accountsFromLocalStorage = getAccountsFromLocalStorage(user)
+            if(!areValuesDecrypted) {
+              setAllAccounts(accountsFromLocalStorage)
+            }
+          }
+          
+          
+        }
+        
+      }
+  
+    }
+
+    if(mnemonic){
+      SetAccountsUseEffectFunction()
+    }
+   
+
+    // if (isLoaded && accounts) {
+    //   console.log("Accounts in Wallet Context:", mnemonic);
+    // }
+  }, [isLoaded, mnemonic]);
+
+
   const contextValue: WalletContextType = {
     selectedNetwork,
     selectedAccount,
@@ -99,17 +407,25 @@ export function WalletContextProvider({
     transferFromAccountId,
     isAddAccountModalOpen,
     mnemonic,
+    transactions,
+    tokens,
+    areValuesDecrypted,
+    password,
+    setWalletPassword,
     changeNetwork,
     changeSelectedAccount,
     setActiveTab,
     toggleGenericTransferModal,
     setTransferFromAccountModal,
     toggleAddAccountModal,
-    setMnemonic,
+    setMnemonicString,
     clearMnemonic,
     addAccount,
     setAllAccounts,
     updateAccountBalance,
+    setAllTokens,
+    setValuesToDecryptedState,
+    setValuesToEncryptedState,
   };
 
   return (
