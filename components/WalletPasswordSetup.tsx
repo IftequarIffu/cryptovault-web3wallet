2f0e99d87bd7ac1b244fed2cbee904c49a2b2aef
IftequarIffu <iftequar.154@gmail.com>
2024-08-16 09:08:41 +0530

    WalletPasswordSetup

--- components/WalletPasswordSetup.tsx
+++ components/WalletPasswordSetup.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+'use client'
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -11,6 +12,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { AlertCircle } from "lucide-react";
+import { useWallet } from "@/context/WalletContext";
+import { useUser } from "@clerk/nextjs";
+import { addHashedPasswordToLocalStorageAndEncryptLocalStorage, getAccountsFromLocalStorage, getEthAndSolAccountsAfterGenerating, getMnemonicAfterGeneratingIt, getMnemonicFromLocalStorage, updateUnsafeMetaDataOfUserForEthAndSol } from "@/lib/utils";
+
 
 interface WalletPasswordSetupProps {
   onComplete: (password: string) => void;
@@ -23,7 +28,123 @@ export default function WalletPasswordSetup({
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const { mnemonic, accounts, setValuesToEncryptedState, setMnemonicString, setAllAccounts } = useWallet();
+  const { user, isLoaded } = useUser();
+
+
+
+  //   const UseEffectFunction = async() => {
+  //   // if (user?.id) {
+  //   //   setSelectedAccount("Eth Account 1")
+  //   //   // setSelectedAccount("account-0")
+  //   //   let storedData = JSON.parse(localStorage.getItem(`user_details_${user?.id}`) || "{}");
+  //   //   if (storedData.mnemonic) {
+  //   //     console.log("Retrieved stored mnemonic for user:", user.id);
+  //   //     setMnemonic(storedData.mnemonic);
+  //   //     if(storedData.accounts.length != 0){
+
+  //   //       let accountsWithUpdatedBalances:any = []
+  //   //       await storedData.accounts.map(async(item: any) => {
+  //   //       const balance = await getBalanceOfAnAddress(item.address, item.network)
+  //   //       console.log("balance: ", balance)
+  //   //       accountsWithUpdatedBalances.push({...item, balance: balance})
+  //   //     })
+
+        
+  //   //     console.log("Accounts with updated balances", accountsWithUpdatedBalances)
+  //   //     localStorage.setItem(`user_details_${user?.id}`, JSON.stringify({mnemonic: storedData.mnemonic, accounts: accountsWithUpdatedBalances}))
+  //   //     setAccounts(accountsWithUpdatedBalances);
+  //   //     }
+  //   //   } else {
+  //   //     const newMnemonic = generateMnemonic();
+  //   //     console.log("Generating new mnemonic for user:", user.id);
+  //   //     setMnemonic(newMnemonic);
+  //   //     const ethAccountPayload = createEthAccount(0, newMnemonic)
+  //   //     const solAccountPayload = await createSolAccount(0, newMnemonic)
+  //   //     const accountsArr = [ethAccountPayload, solAccountPayload]
+  //   //     // setAccounts(getAccountsOfAUserWallet("abcd"))
+  //   //     try {
+  //   //       await initializeLocalStorageWithMnemonicAndEthAccountAndSolAccount(newMnemonic, accountsArr)
+  //   //       await updateAccountMetaData(user)
+  //   //     } catch (error) {
+  //   //       console.log("Error in Wallet Context UseEffect: ", error)
+          
+  //   //     }
+  //   //   }
+  //   // }
+
+  //   if(user?.id) {
+
+  //     // Mnemonic Functions
+  //     const storedData = JSON.parse(localStorage.getItem(`user_details_${user.id}`) || "{}")
+  //     let generatedMnemonic = '';
+
+  //     if(!storedData.mnemonic){
+  //       generatedMnemonic = getMnemonicAfterGeneratingIt(user)
+  //       console.log("Mnemonic after generating: ", generatedMnemonic)
+
+  //       // encryptStringWithPassword(generatedMnemonic, pass)
+  //       setMnemonicString(generatedMnemonic)
+  //     }
+  //     else{
+  //       const mnemonicFromLocalStorage = getMnemonicFromLocalStorage(user)
+  //       setMnemonicString(mnemonicFromLocalStorage)
+  //     }
+
+
+  //     // Account Functions
+  //     let generatedAccounts = []
+  //     if(!storedData.accounts || storedData.accounts.length == 0){
+  //       generatedAccounts =  await getEthAndSolAccountsAfterGenerating(user, generatedMnemonic)
+  //       setAllAccounts(generatedAccounts)
+  //       updateUnsafeMetaDataOfUserForEthAndSol(user, 2, 2)
+  //     }
+  //     else{
+  //       const accountsFromLocalStorage = getAccountsFromLocalStorage(user)
+  //       setAllAccounts(accountsFromLocalStorage)
+  //     }
+
+  //     // Write similar logic for Tokens, NFTs and Recent Tansactions
+      
+  //   }
+
+  // }
+
+  // useEffect(() => {
+
+  //     const SetAccountsUseEffectFunction = async() => {
+    
+  //       if(user?.id) {
+    
+  //         console.log("Yoo! mnemonic: ", mnemonic)
+  
+  //         const storedData = JSON.parse(localStorage.getItem(`user_details_${user.id}`) || "{}")
+    
+  //         // Account Functions
+  //         let generatedAccounts = []
+  //         if(!storedData.accounts || storedData.accounts.length == 0){
+  //           generatedAccounts =  await getEthAndSolAccountsAfterGenerating(user, mnemonic)
+  //           setAllAccounts(generatedAccounts)
+  //           updateUnsafeMetaDataOfUserForEthAndSol(user, 2, 2)
+  //         }
+  //         else{
+  //           const accountsFromLocalStorage = getAccountsFromLocalStorage(user)
+  //           setAllAccounts(accountsFromLocalStorage)
+  //         }
+          
+  //       }
+    
+  //     }
+  
+  //     SetAccountsUseEffectFunction()
+  
+  //     if (isLoaded && accounts) {
+  //       console.log("Accounts in MnemonicDisplay:", mnemonic);
+  //     }
+  //   }, [isLoaded]);
+
+
+  const handleSubmit = async(e: React.FormEvent) => {
     e.preventDefault();
     console.log("Hello from handlesubmit");
     if (password !== confirmPassword) {
@@ -34,6 +155,20 @@ export default function WalletPasswordSetup({
       setError("Password must be at least 8 characters long");
       return;
     }
+
+    // await UseEffectFunction()
+    
+
+    addHashedPasswordToLocalStorageAndEncryptLocalStorage(password, mnemonic, user)
+
+    setValuesToEncryptedState()
+    
+    const browserStorage = JSON.parse(localStorage.getItem(`user_details_${user?.id}`) || "{}")
+    console.log("Browser storage in Wallet Password Setup: ", browserStorage)
+    setMnemonicString(browserStorage.mnemonic)
+    setAllAccounts(browserStorage.accounts)
+
+    // localStorage.setItem(user?.id as string, JSON.stringify(mnemonic));
     onComplete(password);
   };
 
@@ -84,3 +219,4 @@ export default function WalletPasswordSetup({
     </Card>
   );
 }
+
