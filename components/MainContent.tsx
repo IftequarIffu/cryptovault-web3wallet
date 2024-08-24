d1888b27b23f95a483bbc7e517e880ea9dee3360
IftequarIffu <iftequar.154@gmail.com>
2024-08-24 11:46:14 +0530

    Added MainContent

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Accounts } from "./Accounts";
+import React, { Suspense, lazy } from "react";
+// import { Accounts } from "./Accounts";
 import { Overview } from "./Overview";
 import { RecentTransactions } from "./RecentTransactions";
+import { AccountsSkeleton } from "./AccountsSkeleton";
+
+const Accounts = lazy(() => import('@/components/Accounts'));
 
 const MainContent = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <Overview />
-      <Accounts />
-      <RecentTransactions />
+      <Suspense fallback={<AccountsSkeleton />}>
+        <Accounts />
+      </Suspense>
+      {/* <RecentTransactions /> */}
     </div>
   );
 };
