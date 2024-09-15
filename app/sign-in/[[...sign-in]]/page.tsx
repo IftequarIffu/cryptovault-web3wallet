25ac6b50f1e9c81d5709512fa77d53c6aa140849
IftequarIffu <iftequar.154@gmail.com>
2024-09-15 09:15:46 +0530

    Added sign-in

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import OAuthSignIn from "@/components/OAuthSignIn";
+
+
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <OAuthSignIn />
+    </div>
+  );
+}
