1b73fec2605424ffedd07dbb6728deed0420f017
IftequarIffu <iftequar.154@gmail.com>
2024-09-17 09:04:35 +0530

    Added root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { WalletContextProvider } from "@/context/WalletContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,20 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div className="flex justify-center items-center">
-              <div className="w-3/5 p-4">{children}</div>
-            </div>
-          </ThemeProvider>
-        </body>
-      </html>
+    <WalletContextProvider>
+        <html lang="en">
+          <body className={inter.className}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <div className="flex min-h-screen justify-center items-center">
+                {children}
+                {/* <div className="w-3/5 p-4 ">{children}</div> */}
+              </div>
+            </ThemeProvider>
+          </body>
+        </html>
+    </WalletContextProvider>
     </ClerkProvider>
   );
 }
