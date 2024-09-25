177ca5df24fc70ca50b97cf9b7b0cca1d1b68445
IftequarIffu <iftequar.154@gmail.com>
2024-09-25 13:24:57 +0530

    Cleaned Sign Up

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,7 @@
-// import OAuthSignIn from "@/components/OAuthSignIn";
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    // <div className="flex items-center justify-center min-h-screen">
-    //   <OAuthSignIn />
-    // </div>
 
     <SignUp signInUrl="/sign-in" />
   );
