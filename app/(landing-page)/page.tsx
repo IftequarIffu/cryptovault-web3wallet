a294cbd531fd7ecd9772df76e6043762651dfa35
IftequarIffu <iftequar.154@gmail.com>
2024-10-01 11:32:00 +0530

    Modified Landing page

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -7,10 +7,20 @@ import LandingPage from "@/components/LandingPage";
 import { useSignIn } from "@clerk/nextjs";
 import OAuthSignIn from "@/components/OAuthSignIn";
 import { useUser } from "@clerk/nextjs";
+// import { useRouter } from "next/navigation";
+// import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const { user, isLoaded } = useUser()
+
+  if(isLoaded && user) {
+    router.push("/dashboard")
+  }
 
 
   return (
