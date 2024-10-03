420d61cfcb48b1d3625bdd84de3bb04804eb0d74
IftequarIffu <iftequar.154@gmail.com>
2024-10-03 13:43:45 +0530

    Modified Overview

--- components/TotalFunds.tsx
+++ components/TotalFunds.tsx
@@ -1,10 +1,25 @@
-const TotalFunds = () => {
+import { getPricesOfEthAndSolInUSD } from "@/actions/priceActions";
+import { NetworkType } from "@/lib/types";
+
+const TotalFunds = ({ currency, networkType, selectedNetwork }: { currency: string, networkType: string, selectedNetwork: NetworkType }) => {
+
+
+  // const {ethPriceInUSD, solPriceInUSD} = await getPricesOfEthAndSolInUSD()
+
+
+
   return (
     <div className="text-right">
       <p className="text-2xl font-bold">
-        {"0"} {"ETH"}
+        {"0"} {currency}
       </p>
-      <p className="text-sm text-gray-500">${0} USD</p>
+      <p className="text-sm text-gray-500">
+
+        {
+          networkType != "mainnet" ? "0 USD" : "1 USD"
+        }
+        
+        </p>
     </div>
   );
 };
