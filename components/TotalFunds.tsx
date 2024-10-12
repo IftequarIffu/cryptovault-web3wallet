0d5f8a329c98e98cf77724a1c7a2f371bc2e6a68
IftequarIffu <iftequar.154@gmail.com>
2024-10-12 19:25:14 +0530

    Cleaned TotalFunds

--- components/TotalFunds.tsx
+++ components/TotalFunds.tsx
@@ -8,42 +8,22 @@ import { getSumOfBalancesOfAccounts } from "@/actions/balance";
 
 const TotalFunds = () => {
 
-
-  // const {ethPriceInUSD, solPriceInUSD} = await getPricesOfEthAndSolInUSD()
-
   const [totalBalance, setTotalBalance] = useState(0)
-  const [ethAndSolPricesInUSD, setEthAndSolPricesInUSD] = useState({})
   const [ethPriceInUSD, setEthPriceInUSD] = useState(0)
   const [solPriceInUSD, setSolPriceInUSD] = useState(0)
-  // const [totalBalanceInUSD, setTotalBalanceInUSD] = useState(0)
   const [currencyPrices, setCurrencyPrices] = useState<{ ethPriceInUSD: number; solPriceInUSD: number } | null>(null);
 
   const getNetworkInfo = (network: NetworkType) =>
     supportedNetworks[network as NetworkType];
 
   const {
-    tokens,
     accounts,
     selectedNetwork,
-    selectedAccount,
-    changeNetwork,
-    mnemonic,
-    areValuesDecrypted
   } = useWallet();
 
   const selectedNetworkAccounts = accounts.filter((account) => account.network == selectedNetwork)
   console.log("SelectedNetworkAccounts: ", selectedNetworkAccounts)
 
-  // useEffect(() => {
-
-  //   const getEthAndSolPricesInUSD = async() => {
-  //     const currencyPrices = await getPricesOfEthAndSolInUSD()
-  //     setCurrencyPrices(currencyPrices)
-  //   }
-
-  //   getEthAndSolPricesInUSD()
-
-  // }, [])
 
 
 
@@ -57,19 +37,7 @@ const TotalFunds = () => {
         setCurrencyPrices(currencyPrices); // Store prices in state
         setEthPriceInUSD(currencyPrices.ethPriceInUSD);
         setSolPriceInUSD(currencyPrices.solPriceInUSD)
-      }
-      // // const currencyPrices = await getPricesOfEthAndSolInUSD()
-      // if(selectedNetwork == "ETH Sepolia" || selectedNetwork == "Ethereum") {
-      //   const totalBalanceInUSD = totalBalance*(currencyPrices.ethPriceInUSD)
-      //   console.log("TotalBalanceInUSD in useEffect: ", totalBalanceInUSD)
-      //   setTotalBalanceInUSD(totalBalanceInUSD)
-      // }
-      // else{
-      //   const totalBalanceInUSD = totalBalance*(currencyPrices.solPriceInUSD)
-      //   console.log("TotalBalanceInUSD in useEffect: ", totalBalanceInUSD)
-      //   setTotalBalanceInUSD(totalBalanceInUSD)
-      // }
-      
+      }    
 
       setTotalBalance(totalBalance);
     }
@@ -86,16 +54,12 @@ const TotalFunds = () => {
   return (
     <div className="text-right">
       <p className="text-2xl font-bold">
-        {/* {"0"}  */}
         {parseFloat(totalBalance.toPrecision(3)) + " "}
         {getNetworkInfo(selectedNetwork).symbol}
       </p>
       <p className="text-sm text-gray-500">
 
         {
-          // getNetworkInfo(selectedNetwork).type == "mainnet" ? "0 USD" 
-          // : 
-          // "3 USD"
           
           `${parseFloat(totalBalanceInUSD.toFixed(3))} USD`
         }
