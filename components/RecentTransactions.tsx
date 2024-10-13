aa92fedb2a2e51e0296a2571d461bdb12ba7ac46
IftequarIffu <iftequar.154@gmail.com>
2024-10-13 19:22:37 +0530

    Removed some unwanted code

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -59,49 +59,6 @@ export function RecentTransactions() {
       </CardHeader>
       <CardContent>
         <div className="space-y-4 h-60">
-          {/* {transactions?.map((tx, index) => (
-            <div
-              key={index}
-              className="flex justify-between items-center p-2 bg-muted rounded-lg"
-            >
-              <div className="flex items-center">
-                {tx.type === "send" && (
-                  <ArrowUpRight className="h-4 w-4 mr-2 text-red-500" />
-                )}
-                {tx.type === "receive" && (
-                  <ArrowDownRight className="h-4 w-4 mr-2 text-green-500" />
-                )}
-                {tx.type === "swap" && (
-                  <Repeat className="h-4 w-4 mr-2 text-blue-500" />
-                )}
-                <div>
-                  <p className="font-semibold">
-                    {tx.type === "send"
-                      ? "Sent"
-                      : tx.type === "receive"
-                      ? "Received"
-                      : "Swapped"}{" "}
-                    {tx.amount}
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    {tx.type === "send"
-                      ? `To: ${tx.to}`
-                      : tx.type === "receive"
-                      ? `From: ${tx.from}`
-                      : `To: ${tx.to}`}
-                  </p>
-                </div>
-              </div>
-              <div className="text-right">
-                <p className="text-sm text-gray-500">{tx.date}</p>
-                <Button variant="ghost" size="sm">
-                  <ExternalLink className="h-4 w-4 mr-1" /> View
-                </Button>
-              </div>
-            </div>
-          ))} */}
-
-          {/* transactionsToBeShown.length == 0 ?  */}
 
           {
           transactionsToBeShown?.length == 0 ? <div className="font-semibold text-center flex justify-center items-center h-full"><p>No Transactions found</p></div> : 
